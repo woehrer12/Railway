09b00d21366835c75e9784411d68f1f187be4906
woehrer12 <Julian.woehrer@gmail.com>
2021-02-15 20:13:02 +0100

    Add files via upload

--- Eisenbahn.py
+++ Eisenbahn.py
@@ -3,18 +3,10 @@
 '''
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
-import smbus
+
 import functions
-
-address1 = 0x20
-address2 = 0x21
-address3 = 0x22
-address4 = 0x23
-
-gleisebyte1 = 0x00
-gleisebyte2 = 0x00
-
-bus = smbus.SMBus(1)
+import threading
+import time
 
 Gleis1Signal = "unbekannt"
 Gleis2Signal = "unbekannt"
@@ -27,6 +19,11 @@ Gleis8Signal = "unbekannt"
 
 
 app = Flask(__name__)
+
+def loop():
+	while True:
+		print("Looping")
+		time.sleep(1)
 
 @app.route("/")
 def index():
@@ -55,98 +52,58 @@ def action(deviceName, action):
 		if action == "HP1":
 			functions.gleis1_hp1()
 
-
 	if deviceName == 'Gleis2':
 		print("Gleis2")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000000000010
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis2_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000011111101
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-
+			functions.gleis2_hp1()
 
 	if deviceName == 'Gleis3':
 		print("Gleis3")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000000000100
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis3_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000011111011
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
+			functions.gleis3_hp1()
 			
-
 	if deviceName == 'Gleis4':
 		print("Gleis4")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000000001000
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis4_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000011110111
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
+			functions.gleis4_hp1()
 			
-
 	if deviceName == 'Gleis5':
 		print("Gleis5")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000000010000
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis5_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000011101111
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-
+			functions.gleis5_hp1()
 
 	if deviceName == 'Gleis6':
 		print("Gleis6")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000000100000
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis6_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000011011111
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-
+			functions.gleis6_hp1()
 
 	if deviceName == 'Gleis7':
 		print("Gleis7")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000001000000
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis7_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000010111111
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-
+			functions.gleis7_hp1()
 
 	if deviceName == 'Gleis8':
 		print("Gleis8")
 		if action == "HP0":
-			gleisebyte1 = gleisebyte1 | 0b0000000010000000
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
-		
+			functions.gleis8_hp0()
 		if action == "HP1":
-			gleisebyte1 = gleisebyte1 & 0b0000000001111111
-			print(gleisebyte1)
-			bus.write_byte(address1,gleisebyte1)
+			functions.gleis8_hp1()
 
-
+	if deviceName == 'Start':
+		t1 = threading.Thread(target=loop())
+		t1.start()
 
    
 
@@ -163,9 +120,9 @@ def action(deviceName, action):
               'Gleis8'  : Gleis8Signal
 	}
 	return render_template('index.html', **templateData)
+	
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=3000, debug=True)
-
+	app.run(host='0.0.0.0', port=3000, debug=True)
 
 #Bitoperationen
 #https://www.webmasterpro.de/coding/article/bit-operationen-in-python.html
--- functions.py
+++ functions.py
@@ -26,3 +26,91 @@ def gleis1_hp1():
 	gleisebyte1 = gleisebyte1 & 0b0000000011111110
 	print(gleisebyte1)
 	bus.write_byte(address1,gleisebyte1)
+	
+def gleis2_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000000000010
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+	
+def gleis2_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000011111101
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis3_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000000000100
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+	
+def gleis3_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000011111011
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis4_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000000001000
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis4_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000011110111
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+	
+def gleis5_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000000010000
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)	
+	
+def gleis5_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000011101111
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis6_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000000100000
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis6_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000011011111
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis7_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000001000000
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis7_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000010111111
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis8_hp0():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 | 0b0000000010000000
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+def gleis8_hp1():
+	global gleisebyte1
+	gleisebyte1 = gleisebyte1 & 0b0000000001111111
+	print(gleisebyte1)
+	bus.write_byte(address1,gleisebyte1)
+
+
+
+
